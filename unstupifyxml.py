3c997ba9f784f714af5c4627e56cfc1b7cdea654
Greg Velichansky <hmaon@bumba.net>
2018-05-30 00:37:49 -0400

    optimized unstupifyxml.py

--- unstupifyxml.py
+++ unstupifyxml.py
@@ -4,20 +4,15 @@ import sys
 import re
 
 
-exp = re.compile(r'<([^/ >]*)[^>]*>')
+exp = re.compile(r'<([^/ >]*)[^>]*>[^<]*')
 s = sys.stdin.read()
 stack = []
 
-while len(s) > 0:
-    res = exp.search(s)
-    if res == None:
-        sys.stdout.write(s)
-        break
-    sys.stdout.write(s[:res.start()])
+
+for res in exp.finditer(s):
     tag = res.group()
-    if tag == '</>' and len(stack) > 0:
-        tag = stack.pop()
-    elif tag.count('/>') == 0 or tag.find("<svg") == 0:
+    if tag.startswith('</>') and len(stack) > 0:
+        tag = tag.replace('</>', stack.pop())
+    elif tag.count('/>') == 0 or tag.startswith("<svg"):
         stack.append('</' + res.group(1) + '>')
     sys.stdout.write(tag)
-    s = s[res.end():]
